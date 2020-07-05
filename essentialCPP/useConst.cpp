51cb3a5dda4a1603c05546baf4cde9d474c04d56
byt <1564047446@qq.com>
2020-07-05 20:47:14 +0800

    useCount add method

--- essentialCPP/useConst.cpp
+++ essentialCPP/useConst.cpp
@@ -20,18 +20,31 @@ public:
     int getCle() const;
     // 可以修改成员变量
     void setR(int newR);
-    
+    const Shape* getShape() const;
+    Shape* getShape();
+
 private:
     int r;
     int area;
     int cle;
+    Shape shape;
 };
+
+
 
 int Circle::getR() const { return r; }
 int Circle::getArea() const { return area; }
 int Circle::getCle() const { return cle; }
 void Circle::setR(int newR) { r = newR; }
+const Shape* Circle::getShape() const {
+    std::cout << "const func" << std::endl;
+    return nullptr;
+}
 
+Shape* Circle::getShape() {
+    std::cout << "normal func" << std::endl;
+    return nullptr;
+}
 
 int main()
 {
@@ -41,5 +54,11 @@ int main()
     std::cout << "r = " << circle.getR() << std::endl;
     circle.setR(10);
     std::cout << "newR = " << circle.getR() << std::endl;
+    const Circle circle3;
+    // 会调用const版本
+    circle3.getShape();
+    Circle circle4;
+    // 会调用normal版本
+    circle4.getShape();
     return 0;
 }
